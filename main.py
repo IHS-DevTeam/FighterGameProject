73fc1a27846027a4a23ca1b135a16a2ba696454c
danchoi0304 <danchoi0304@gmail.com>
2024-08-12 12:04:42 -0400

    Added character selection page template and idle animation

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha
 #load start page images
 start_img = pygame.image.load("assets/images/Start Page/start.png").convert_alpha()
 title_img = pygame.image.load("assets/images/Start Page/title.png").convert_alpha()
+frame_img = pygame.image.load("assets/images/Selection Page/frame.png").convert_alpha()
 
 #define number of steps in each animation
 WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
@@ -102,13 +103,55 @@ def draw_start_page(title_img, start_img):
     screen.blit(start_img, (SCREEN_WIDTH // 2 - start_img.get_width() // 2, 300))
 
 #function for drawing character selection page
-def draw_character_selection_page(start_img):
+def draw_character_selection_page(start_img, frame_img):
     screen.fill((0, 0, 0))
 
     start_width = 200  # desired width
     start_height = int(start_img.get_height() * (start_width / start_img.get_width()))
     start_img = pygame.transform.smoothscale(start_img, (start_width, start_height))
-    screen.blit(start_img, (SCREEN_WIDTH // 2 - start_img.get_width() // 2, 300))
+    screen.blit(start_img, (SCREEN_WIDTH // 2 - start_img.get_width() // 2, 400))
+
+    frame_width = 200  # desired width
+    frame_height = int(frame_img.get_height() * (frame_width / frame_img.get_width()))
+    frame_img = pygame.transform.smoothscale(frame_img, (frame_width, frame_height))
+    screen.blit(frame_img, (SCREEN_WIDTH // 3 - frame_img.get_width() // 2, 100))
+    screen.blit(frame_img, (2 * SCREEN_WIDTH // 3 - frame_img.get_width() // 2, 100))
+
+    warrior_idle_sheet = pygame.image.load("assets/images/warrior/Sprites/idle.png").convert_alpha()
+    warrior_idle_frames = idle_animation(warrior_idle_sheet, 10)
+
+    wizard_idle_sheet = pygame.image.load("assets/images/wizard/Sprites/idle.png").convert_alpha()
+    wizard_idle_frames = idle_animation(wizard_idle_sheet, 8)
+
+    # Draw idle animations for characters
+    frame_rate = 10  # Frames per second for animation
+    animation_time = pygame.time.get_ticks() // frame_rate
+    warrior_frame_index = (animation_time // frame_rate) % len(warrior_idle_frames)
+    wizard_frame_index = (animation_time // frame_rate) % len(wizard_idle_frames)
+
+    # Blit the idle frames for both characters
+    warrior_frame = warrior_idle_frames[warrior_frame_index]
+    wizard_frame = wizard_idle_frames[wizard_frame_index]
+
+    # Double the size of the idle frames
+    warrior_frame = pygame.transform.scale(warrior_frame, (warrior_frame.get_width() * 2.5, warrior_frame.get_height() * 2.5))
+    wizard_frame = pygame.transform.scale(wizard_frame, (wizard_frame.get_width() * 2, wizard_frame.get_height() * 2))
+
+    # Draw the frames on the screen
+    screen.blit(warrior_frame, (SCREEN_WIDTH // 3 - warrior_frame.get_width() // 2, 30))
+    screen.blit(wizard_frame, (2 * SCREEN_WIDTH // 3 - wizard_frame.get_width() // 2, -50))
+
+    pygame.display.update()
+
+#make idle-ing animation for character selection page
+def idle_animation(idle_sprite_sheet, animation_steps):
+    idle_animation_list = []
+    sheet_width, sheet_height = idle_sprite_sheet.get_size()
+    frame_width = sheet_width // animation_steps
+    for i in range(animation_steps):
+        temp_img = idle_sprite_sheet.subsurface(pygame.Rect(i * frame_width, 0, frame_width, sheet_height))
+        idle_animation_list.append(temp_img)
+    return idle_animation_list
 
 #create two instances of fighters
 fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
@@ -132,7 +175,7 @@ while run:
 
                 #checks for character selection
                 while not character_selected:
-                    draw_character_selection_page(start_img)
+                    draw_character_selection_page(start_img, frame_img)
                     pygame.display.update()
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
