e6aaa0b12492f4a41323a88297103580a311da48
danchoi0304 <danchoi0304@gmail.com>
2024-08-16 21:19:12 -0400

    Made it so that player 2 can play as wizard (flip should be variable not constant)

--- characters/warrior.py
+++ characters/warrior.py
@@ -20,4 +20,4 @@ WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
 class Warrior(Fighter):
   def __init__(self, player, cord, flip):
     Spawn_X, Spawn_Y = cord
-    super().__init__(player, Spawn_X, Spawn_Y, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+    super().__init__(player, Spawn_X, Spawn_Y, flip, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
--- characters/wizzard.py
+++ characters/wizzard.py
@@ -27,4 +27,4 @@ WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 class Wizard(Fighter):
   def __init__(self, player, cord, flip):
     Spawn_X, Spawn_Y = cord
-    super().__init__(player, Spawn_X, Spawn_Y, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+    super().__init__(player, Spawn_X, Spawn_Y, flip, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
