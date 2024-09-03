6b5fe1ab4836bea612e84724cc48d1f01cc10e1b
danchoi0304 <danchoi0304@gmail.com>
2024-09-03 15:37:24 -0400

    Added spritesheet for new character

---
+++ characters/huntress.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame import mixer
+
+mixer.init()
+pygame.init()
+
+from util.fighter import Fighter
+
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+
+#musics
+sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+sword_fx.set_volume(0.5)
+warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
+
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+class Warrior(Fighter):
+  def __init__(self, player, cord, flip, isAI):
+    
+    super().__init__(player, cord, flip, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx, isAI)
