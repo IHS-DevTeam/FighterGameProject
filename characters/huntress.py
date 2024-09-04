ff43ac242b007c02a922bebdee45a3fe89a11a4a
danchoi0304 <danchoi0304@gmail.com>
2024-09-03 20:47:05 -0400

    Update huntress.py

--- characters/huntress.py
+++ characters/huntress.py
@@ -19,7 +19,7 @@ sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
 sword_fx.set_volume(0.5)
 huntress_sheet = pygame.image.load("assets/images/huntress/Sprites/huntress.png").convert_alpha()
 
-HUNTRESS_ANIMATION_STEPS = [10, 8, 2, 6, 0, 3, 10]
+HUNTRESS_ANIMATION_STEPS = [10, 8, 2, 6, 2, 3, 10]
 class Huntress(Fighter):
   def __init__(self, player, cord, flip, isAI):
     
